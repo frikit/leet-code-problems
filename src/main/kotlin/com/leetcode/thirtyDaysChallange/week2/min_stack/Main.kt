93d54dd0cf041d61d863f23525990abd9110f169
frikit <osipovvictor1994@gmail.com>
2020-04-12 12:20:21 +0100

    sp;ve min stack problem

---
+++ src/main/kotlin/com/leetcode/thirtyDaysChallange/week2/min_stack/Main.kt
@@ -0,0 +1,62 @@
+package com.leetcode.thirtyDaysChallange.week2.min_stack
+
+import java.util.*
+
+fun main() {
+    val minStack = MinStack()
+    minStack.push(-2)
+    println(minStack.getMin() == -2)
+    println(minStack.top() == -2)
+    println()
+
+    minStack.push(0)
+    println(minStack.getMin() == -2)
+    println(minStack.top() == 0)
+    println()
+
+    minStack.push(-3)
+    println(minStack.getMin() == -3)
+    println(minStack.top() == -3)
+    println()
+
+    minStack.push(0)
+    println(minStack.getMin() == -3)
+    println(minStack.top() == 0)
+    println()
+
+    minStack.pop()
+    println(minStack.getMin() == -3)
+    println(minStack.top() == 0)
+    println()
+
+    minStack.pop()
+    println(minStack.getMin() == -2)
+    println(minStack.top() == 0)
+}
+
+class MinStack {
+
+    val min = PriorityQueue<Int>()
+    val stack = Stack<Int>()
+
+    fun push(x: Int) {
+        min.add(x)
+        stack.push(x)
+    }
+
+    fun pop() {
+        val pop = stack.pop()
+        min.remove(pop)
+    }
+
+    fun top(): Int {
+        println(min)
+        println(stack)
+        return stack.peek()
+    }
+
+    fun getMin(): Int {
+        return min.elementAt(0)
+    }
+
+}
