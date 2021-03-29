60f318026525ad1e11456d66787972d8aa6fad0c
frikit <osipovvictor1994@gmail.com>
2021-03-29 20:26:33 +0100

    solve move zeroes problem

---
+++ src/main/kotlin/com/leetcode/top100LikedQuestions/easy/move_zeroes/Main.kt
@@ -0,0 +1,30 @@
+package com.leetcode.top100LikedQuestions.easy.move_zeroes
+
+fun main() {
+    println("Test case 1:")
+    println(Solution().moveZeroes(intArrayOf(0, 1, 0, 3, 12)))
+    println()
+    println("Test case 2:")
+    println(Solution().moveZeroes(intArrayOf(0)))
+    println()
+
+}
+
+class Solution {
+    fun moveZeroes(nums: IntArray): Unit {
+        val final = mutableListOf<Int>()
+        val zeroes = mutableListOf<Int>()
+
+        nums.forEachIndexed { i, number ->
+            if (number == 0) {
+                zeroes.add(0)
+            } else {
+                final.add(number)
+            }
+        }
+
+        (final + zeroes).forEachIndexed { i, e ->
+            nums[i] = e
+        }
+    }
+}
