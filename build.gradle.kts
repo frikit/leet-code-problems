95b6bbe2f7bd6411a7cb43d36dfd65e0d96ecd78
frikit <osipovvictor1994@gmail.com>
2022-08-23 12:33:55 +0100

    bump kotlin 1.6.0 -> 1.7.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     java
-    kotlin("jvm") version "1.6.0"
+    kotlin("jvm") version "1.7.10"
 }
 
 group = "org.github.frikit"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    mavenLocal()
     mavenCentral()
 }
